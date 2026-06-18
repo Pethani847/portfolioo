bb65a2a11705ea2100eb0fbb27b20cd74873eea5
Madhvi Pethani <madhvipethani856@gmail.com>
2026-06-18 16:47:23 +0530

    remove api

--- frontend/src/Contact.jsx
+++ frontend/src/Contact.jsx
@@ -17,7 +17,7 @@ function Contact() {
     };
 
     try {
-      const res = await fetch("https://portfolio-backend-fx66.onrender.com/api/contact", {
+      const res = await fetch("https://portfolio-backend-fx66.onrender.com/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
